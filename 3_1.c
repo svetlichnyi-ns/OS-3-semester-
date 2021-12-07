b25aa218f0dfb8f27f555dfad3daebf323a7e31a
svetlichnyi-ns <80046790+svetlichnyi-ns@users.noreply.github.com>
2021-12-07 18:30:02 +0300

    Update 3_1.c

--- 3_1.c
+++ 3_1.c
@@ -59,35 +59,7 @@ int main(int argc, char* argv[]) {
     }
     int fd_1 = open(argv[1], O_RDONLY, ALLPERMS);
     if (fd_1 < 0) {
-        fprintf(stderr, "Error: cannot open the first file '%s'\n", argv[1]);
-        printf("Name of error: ");
-        switch (errno) {
-            case 13:  printf("EACCES\n");       break;
-            case 122: printf("EDQUOT\n");       break;
-            case 17:  printf("EEXIST\n");       break;
-            case 14:  printf("EFAULT\n");       break;
-            case 27:  printf("EFBIG\n");        break;
-            case 4:   printf("EINTR\n");        break;
-            case 22:  printf("EINVAL\n");       break;
-            case 21:  printf("EISDIR\n");       break;
-            case 40:  printf("ELOOP\n");        break;
-            case 24:  printf("EMFILE\n");       break;
-            case 36:  printf("ENAMETOOLONG\n"); break;
-            case 23:  printf("ENFILE\n");       break;
-            case 19:  printf("ENODEV\n");       break;
-            case 2:   printf("ENOENT\n");       break;
-            case 12:  printf("ENOMEM\n");       break;
-            case 28:  printf("ENOSPC\n");       break;
-            case 20:  printf("ENOTDIR\n");      break;
-            case 6:   printf("ENXIO\n");        break;
-            case 95:  printf("EOPNOTSUPP\n");   break;
-            case 75:  printf("EOVERFLOW\n");    break;
-            case 1:   printf("EPERM\n");        break;
-            case 30:  printf("EROFS\n");        break;
-            case 26:  printf("ETXTBSY\n");      break;
-            case 11:  printf("EWOULDBLOCK\n");  break;
-            default:  printf("unknown\n");      break;
-        }
+        perror("open");
         return RESULT_ERROR_OPENING_FILE;
     }
     FILE* filestream_1 = fdopen(fd_1, "r");
@@ -101,30 +73,12 @@ int main(int argc, char* argv[]) {
     fseek(filestream_1, 0, SEEK_SET);
     ssize_t read_bytes = read_all(fd_1, buffer, size_of_file);
     if (read_bytes < 0) {
-        fprintf(stderr, "Error: cannot read from the first file '%s'\n", argv[1]);
-        printf("Name of error: ");
-        switch (errno) {
-            case 11:  printf("EAGAIN\n");      break;
-            case 9:   printf("EBADF\n");       break;
-            case 14:  printf("EFAULT\n");      break;
-            case 4:   printf("EINTR\n");       break;
-            case 22:  printf("EINVAL\n");      break;
-            case 5:   printf("EIO\n");         break;
-            case 21:  printf("EISDIR\n");      break;
-            default:  printf("unknown\n");     break;
-        }
+        perror("read");
         free(buffer);
         return RESULT_ERROR_READING_FILE;
     }
     if (close(fd_1) < 0) {
-        fprintf(stderr, "Error: cannot close the first file '%s'\n", argv[1]);
-        printf("Name of error: ");
-        switch(errno) {
-            case 9:  printf("EBADF\n");       break;
-            case 4:  printf("EINTR\n");       break;
-            case 5:  printf("EIO\n");         break;
-            default: printf("unknown\n");     break;
-        }
+        perror("close");
         free(buffer);
         return RESULT_ERROR_CLOSING_FILE;
     }
@@ -139,35 +93,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     if (fd_2 < 0) {
-        fprintf(stderr, "Error: cannot open the second file '%s'\n", argv[2]);
-        printf("Name of error: ");
-        switch(errno) {
-            case 13:  printf("EACCES\n");       break;
-            case 122: printf("EDQUOT\n");       break;
-            case 17:  printf("EEXIST\n");       break;
-            case 14:  printf("EFAULT\n");       break;
-            case 27:  printf("EFBIG\n");        break;
-            case 4:   printf("EINTR\n");        break;
-            case 22:  printf("EINVAL\n");       break;
-            case 21:  printf("EISDIR\n");       break;
-            case 40:  printf("ELOOP\n");        break;
-            case 24:  printf("EMFILE\n");       break;
-            case 36:  printf("ENAMETOOLONG\n"); break;
-            case 23:  printf("ENFILE\n");       break;
-            case 19:  printf("ENODEV\n");       break;
-            case 2:   printf("ENOENT\n");       break;
-            case 12:  printf("ENOMEM\n");       break;
-            case 28:  printf("ENOSPC\n");       break;
-            case 20:  printf("ENOTDIR\n");      break;
-            case 6:   printf("ENXIO\n");        break;
-            case 95:  printf("EOPNOTSUPP\n");   break;
-            case 75:  printf("EOVERFLOW\n");    break;
-            case 1:   printf("EPERM\n");        break;
-            case 30:  printf("EROFS\n");        break;
-            case 26:  printf("ETXTBSY\n");      break;
-            case 11:  printf("EWOULDBLOCK\n");  break;
-            default:  printf("unknown\n");      break;
-        }
+        perror("open");
         free(buffer);
         return RESULT_ERROR_OPENING_FILE;
     }
@@ -178,32 +104,12 @@ int main(int argc, char* argv[]) {
     }
     fseek(filestream_2, 0, SEEK_SET);
     if (write_all(fd_2, buffer, strlen(buffer)) < 0) {
-        fprintf(stderr, "Error: cannot write to the second file '%s'\n", argv[2]);
-        printf("Name of error: ");
-        switch(errno) {
-            case 9:   printf("EBADF\n");        break;
-            case 22:  printf("EINVAL\n");       break;
-            case 14:  printf("EFAULT\n");       break;
-            case 27:  printf("EFBIG\n");        break;
-            case 32:  printf("EPIPE\n");        break;
-            case 11:  printf("EAGAIN\n");       break;
-            case 4:   printf("EINTR\n");        break;
-            case 28:  printf("ENOSPC\n");       break;
-            case 5:   printf("EIO\n");          break;
-            default:  printf("unknown\n");      break;
-        }
+        perror("write");
         free(buffer);
         return RESULT_ERROR_WRITING_IN_FILE;
     }
     if (close(fd_2) < 0) {
-        fprintf(stderr, "Error: cannot close the second file '%s'\n", argv[2]);
-        printf("Name of error: ");
-        switch(errno) {
-            case 9:  printf("EBADF\n");       break;
-            case 4:  printf("EINTR\n");       break;
-            case 5:  printf("EIO\n");         break;
-            default: printf("unknown\n");     break;
-        }
+        perror("close");
         free(buffer);
         return RESULT_ERROR_CLOSING_FILE;
     }
