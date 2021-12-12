c13ab040476907e64a4e805f4be29f8925657a25
svetlichnyi-ns <80046790+svetlichnyi-ns@users.noreply.github.com>
2021-12-12 16:10:53 +0300

    Update 6_1.c

--- 6_1.c
+++ 6_1.c
@@ -8,7 +8,7 @@
 #include <fcntl.h>
 #include <errno.h>
 
-char d_type_char (unsigned char d_type) {
+char d_type_char (unsigned char d_type) {   // this function identificates type of file, using field "d_type" of the struct dirent, filled in by function "readdir"
     switch (d_type) {
         case DT_BLK:     return 'b'; break;
         case DT_CHR:     return 'c'; break;
@@ -22,7 +22,7 @@ char d_type_char (unsigned char d_type) {
     return '?';
 }
 
-char mode_char (unsigned mode) {
+char mode_char (unsigned mode) {   // this function identificates type of file, using field "stx_mode" of the struct statx, filled in by function "statx"
     switch (mode & S_IFMT) {
         case S_IFBLK:     return 'b'; break;
         case S_IFCHR:     return 'c'; break;
@@ -36,16 +36,18 @@ char mode_char (unsigned mode) {
 }
 
 int main(void) {
-    DIR* dir_stream = opendir(".");
+    DIR* dir_stream = opendir(".");   // get a pointer to the stream of the current working directory
     if (dir_stream == NULL) {
         perror("opendir");
         return -1;
     }
     struct dirent* entry;
     struct statx sbx;
-    while ((entry = readdir(dir_stream)) != NULL) {
+    while ((entry = readdir(dir_stream)) != NULL) {  // if the directory has not ended yet...
+        /* Function "readdir" may also return a zero pointer if directory stream descriptor is invalid. 
+        But we are sure in its validity, because we've already handled possible errors, returned by "opendir" */ 
         char entry_type = d_type_char(entry->d_type);
-        if (entry_type == '?') {
+        if (entry_type == '?') {   // if field "d_type" didn't provide us with information about filetype, we use statx(2)
             if (statx(dirfd(dir_stream), entry->d_name, AT_SYMLINK_NOFOLLOW, STATX_TYPE, &sbx) == -1) {
                 perror("statx");
                 return -1;
@@ -54,6 +56,6 @@ int main(void) {
         }
         printf("%c %s\n", entry_type, entry->d_name); 
     }
-    closedir(dir_stream);
+    closedir(dir_stream); // errors are also impossible here
     return 0;
 }
